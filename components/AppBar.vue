985c6a000fb6611a8d87c7e953cfed35a331620a
mekrograca <grkem.squash99@gmail.com>
2023-02-01 18:01:49 +0300

    first commit

---
+++ components/AppBar.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+const navLinks = ref<{
+    to: string,
+    label: string,
+}[]>([
+    {
+        to: '/',
+        label: 'Home'
+    },
+    {
+        to: '/about',
+        label: 'About'
+    },
+    {
+        to: '/contact',
+        label: 'Contact'
+    }
+])
+
+</script>
+
+<template>
+    <div class="fixed top-0 left-0 right-0 border-b border-white/20 bg-blur">
+        <nav class="container py-4 flex justify-between">
+            <nuxt-link :to="{ name: 'index' }" class="text-2xl font-bold text-white">
+                shortly
+            </nuxt-link>
+            <ul class="flex items-center space-x-4">
+                <li v-for="link in navLinks" :key="link.to">
+                    <nuxt-link :to="link.to">
+                        {{ link.label }}
+                    </nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/auth" class="btn btn-primary">Sign In</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <section class="h-screen bg-none grid place-content-center">
+            <div class="max-w-3xl mx-auto text-center">
+                <h1 class="text-5xl font-bold heading">Make your links tiny and easy to share with tnyl</h1>
+                <p class="my-10 text-lg w-10/12 md:w-2/3 mx-auto">
+                    Transform your long, cumbersome URLs into short and
+                    shareable links with TinyLink
+                </p>
+                <nuxt-link :to="{ name: 'index' }" class="btn btn-primary">
+                    Short Now!
+                </nuxt-link>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style scoped>
+.heading {
+    background: linear-gradient(180deg, #fff 0%, rgba(6, 255, 89, 0) 189%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+</style>
